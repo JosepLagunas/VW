520efeb7d5ddaacfa33f6eb8ad2d2eb6c530ca47
Josep Lagunas <josep@yukisoftware.es>
2019-06-07 19:45:46 +0200

    tests on build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo branch: $TRAVIS_BRANCH
+echo "start restore"
+#dotnet restore ./src/WebPlatform.sln
+echo "restore done"
+
+echo "start build"
+#dotnet build ./src/WebPlatform.sln
+echo "build done"
+
+function test(){
+	local a=25
+    local b=3
+	local c=$((a+b))
+	echo $c
+}
+test
+
+function get_docker_tag(){
+	if [ $1 == "development" ]; then
+		return $"development-{$2}"
+	else
+		return $"production-{$2}"
+	fi	
+}
+dockerTag=get_docker_tag $TRAVIS_BRANCH $TRAVIS_COMMIT
+echo started building docker image for $dockerTag
+
